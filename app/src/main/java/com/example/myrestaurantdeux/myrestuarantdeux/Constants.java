5c21c51f827f4313a56699e587aa1e59ebf70da5
seravinb <seravinbehnken@gmail.com>
2018-11-05 08:05:13 -0500

    Yelp API working

---
+++ app/src/main/java/com/example/myrestaurantdeux/myrestuarantdeux/Constants.java
@@ -0,0 +1,8 @@
+package com.example.myrestaurantdeux.myrestuarantdeux;
+
+public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurants";
+    public static final String LYELP_LOCATION_QUERY_PARAMETER = "location";
+
+}
---
+++ app/src/main/java/com/example/myrestaurantdeux/myrestuarantdeux/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myrestaurantdeux.myrestuarantdeux;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast erased in lesson plan but I wanted to keep for learning sake.
+                //  Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+
+                String location = mLocationEditText.getText().toString();
+
+                // Logs data in the logcat if we're wanting to get that information
+                //Log.d(TAG, location);
+
+                        //new instance that takes two parameters. Current context (the environment where the code is running),
+                // and the activity class
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+
+                //we attach data to an intent as a key/value pair
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}
